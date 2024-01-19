01bfe1e16e5a047ca1cf571e6b3464af613a55c0
LMH <90649822+LoneHilke@users.noreply.github.com>
2024-01-19 22:37:51 +0100

    beskrivelse virker ikke
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- hotel/beskriv/urls.py
+++ hotel/beskriv/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from .views import Beskriv
+from info.views import Index
 from django.views import View
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-  path('beskriv/', Beskriv.as_view(), name='beskriv'),
+    path('index/', Index.as_view(), name='index'),
+    path('beskriv/', Beskriv.as_view(), name='beskriv'),
   
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hotel/beskriv/views.py
+++ hotel/beskriv/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.views import View
 from info.models import Beskrivelse
 # Create your views here.
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'info/base.html')
+
 class Beskriv(View):
   def get(self, request, *args, **kwargs):
 
